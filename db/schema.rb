b139f5f84d00be606dd01b69984b875763f724c8
Mike Horn <mhorn918@gmail.com>
2015-03-10 15:26:56 -0400

    Update gemfile, database/yml and development.rb settings

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150310054802) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at",                     null: false
@@ -37,7 +40,7 @@ ActiveRecord::Schema.define(version: 20150310054802) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
